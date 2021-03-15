b85c851ec292790253f6f10131e4a88081d15085
vinny <vinny@mydomain.com>
2021-03-15 11:54:14 -0400

    setup of project

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
+const db = mongoose.connection;
+db.on('connected', function() {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+})
+
+
+
+
+
